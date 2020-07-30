ac156738f671790f21f4a1fb8880a106cc1253ad
Alex Shevelkin <ash@tvip.ru>
2020-07-30 16:10:07 +0300

    Pack get-efuse-keys utilities to deb package jethome-get-efuse-keys -pack get-cpuid utility to deb package jethome-get-cpuid -rename helloworld deb package to jethome-helloworld -refactor deb package creating

--- jethome/customize-image.sh
+++ jethome/customize-image.sh
@@ -18,7 +18,7 @@ BUILD_DESKTOP=$4
 
 create_deb_packages() {
   mkdir -v /tmp/overlay/debs
-  for package_script in /tmp/overlay/packages/*; do
+  for package_script in /tmp/overlay/packages/*.conf; do
     source $package_script
   done
 }
@@ -50,22 +50,6 @@ install_fw_env_config_generator() {
 
 }
 
-compile_get_efuse_utilities() {
-  local GET_EFUSE=get_efuse_
-  local RAW_KEY=raw_key
-  local KEY_MAC=key_mac
-  local KEY_USID=key_usid
-
-  g++ -Wall -Wextra -std=c++17 /tmp/overlay/usr/local/bin/get_efuse/${GET_EFUSE}${RAW_KEY}.cpp -o /usr/local/bin/${GET_EFUSE}${RAW_KEY}
-  g++ -Wall -Wextra -std=c++17 /tmp/overlay/usr/local/bin/get_efuse/${GET_EFUSE}${KEY_MAC}.cpp -o /usr/local/bin/${GET_EFUSE}${KEY_MAC}
-  g++ -Wall -Wextra -std=c++17 /tmp/overlay/usr/local/bin/get_efuse/${GET_EFUSE}${KEY_USID}.cpp -o /usr/local/bin/${GET_EFUSE}${KEY_USID}
-}
-
-compile_get_cpuid() {
-  local GET_CPUID=get_cpuid
-  g++ -Wall -Wextra -std=c++17 /tmp/overlay/usr/local/bin/$GET_CPUID.cpp -o /usr/local/bin/$GET_CPUID
-}
-
 Main() {
   case $RELEASE in
     focal)
@@ -74,8 +58,6 @@ Main() {
           create_deb_packages
           create_brcm_symlinks
           install_fw_env_config_generator
-          compile_get_efuse_utilities
-          compile_get_cpuid
         fi
       fi
       ;;
