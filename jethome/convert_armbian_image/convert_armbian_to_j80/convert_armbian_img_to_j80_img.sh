8c40b65b7bd116788c9ceba97260bcf5805d6a74
Alex Shevelkin <ash@tvip.ru>
2020-09-08 14:44:48 +0300

    Fix typo and remove extra logging

--- jethome/convert_armbian_image/convert_armbian_to_j80/convert_armbian_img_to_j80_img.sh
+++ jethome/convert_armbian_image/convert_armbian_to_j80/convert_armbian_img_to_j80_img.sh
@@ -1,6 +1,4 @@
-#/bin/bash -x
-#
-# -x        Print commands and their arguments as they are executed.
+#!/bin/bash
 
 set -e # Exit immediately if a command exits with a non-zero status
 set -u # Treat unset variables and parameters as an error
