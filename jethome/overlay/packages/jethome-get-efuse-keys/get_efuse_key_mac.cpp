01b1703f18b40d326e4094f5ff9daf965fc9e9f3
Alex Shevelkin <ash@tvip.ru>
2021-01-21 14:58:03 +0300

    Fix get_efuse_key_mac output -show 10:27:be:01:01:01 instead of 10:27:be:1:1:1

--- jethome/overlay/packages/jethome-get-efuse-keys/get_efuse_key_mac.cpp
+++ jethome/overlay/packages/jethome-get-efuse-keys/get_efuse_key_mac.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <iomanip>
 
 #include "get_efuse_raw_key.h"
 
@@ -6,7 +7,7 @@ int main (/*int argc, char *argv[]*/) {
   auto mac = getKey("mac");
   if (mac.size() == 6) {
     for (auto it = mac.begin(); it != mac.end(); ++it) {
-      std::cout << std::hex << static_cast<int>(*it);
+      std::cout << std::setfill('0') << std::setw(2) << std::right << std::hex << static_cast<int>(*it);
       if (it < mac.end() - 1) {
         std::cout << ":";
       }
